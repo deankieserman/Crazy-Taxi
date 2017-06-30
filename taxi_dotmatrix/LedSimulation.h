8edf812d92f493bb14dfac6b0c7ac3f3ad75575e
nyurdakul <nazliyurdakul@gmail.com>
2017-06-30 12:25:05 +0300

    Add files via upload

---
+++ taxi_dotmatrix/LedSimulation.h
@@ -0,0 +1,24 @@
+
+#include <Adafruit_GFX.h>    // Core graphics library
+//#include <Adafruit_TFTLCD.h> // Hardware-specific library
+#include <Adafruit_ILI9341.h>
+
+class LedSimulation {
+  private:
+    Adafruit_ILI9341 *pTft;
+    unsigned char Scene[18][16];
+    unsigned char SceneWas[18][16];
+  public:
+    LedSimulation::LedSimulation(Adafruit_ILI9341 &tft);
+    void clear(void);
+    void clearScene(void);
+    void display(void);
+    void draw(void);
+    void setScene(unsigned char Scene[18][16]);
+    void setLedOn(int x, int y);
+    void setLedOff(int x, int y);
+    void setLedBlue(int x, int y);
+    void setLedGreen(int x, int y);
+    void setLedYellow(int x, int y);
+    void setLedRandom(int x, int y);
+};
